6a84aacd6a5c802e49c65fac334e0d4bade07a9f
m4dEngi <m4dengi@gmail.com>
2017-03-26 20:52:15 +0300

    EAppType + Little Steam_RunCallbacks fix

--- OpenSteamworks/AppsCommon.h
+++ OpenSteamworks/AppsCommon.h
@@ -36,8 +36,6 @@
 
 #define STEAMAPPLIST_INTERFACE_VERSION_001 "STEAMAPPLIST_INTERFACE_VERSION001"
 
-
-
 enum EAppState
 {
 	k_EAppStateInvalid = 0,
@@ -97,6 +95,28 @@ enum EAppInfoSection
 	k_EAppInfoSectionSysreqs,
 	k_EAppInfoSectionCommunity,
 	k_EAppInfoSectionStore
+};
+
+enum EAppType
+{
+	k_EAppTypeDepotonly = -2147483648,
+	k_EAppTypeInvalid = 0,
+	k_EAppTypeGame,
+	k_EAppTypeApplication,
+	k_EAppTypeTool = 4,
+	k_EAppTypeDemo = 8,
+	k_EAppTypeMedia = 16,
+	k_EAppTypeDlc = 32,
+	k_EAppTypeGuide = 64,
+	k_EAppTypeDriver = 128,
+	k_EAppTypeConfig = 256,
+	k_EAppTypeHardware = 512,
+	k_EAppTypeFranchise = 1024,
+	k_EAppTypeVideo = 2048,
+	k_EAppTypePlugin = 4096,
+	k_EAppTypeMusic = 8192,
+	k_EAppTypeSeries = 16384,
+	k_EAppTypeShortcut = 1073741824,
 };
 
 #pragma pack( push, 1 )
