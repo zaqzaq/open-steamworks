9b7f956a884303ff22d4ee5a20f12b1084b2ca1b
m4dengi <m4dengi@gmail.com>
2020-06-07 15:57:04 +0300

    Client update 06/06/2020

--- OpenSteamworks/Interfaces/Client/IClientUtils.h
+++ OpenSteamworks/Interfaces/Client/IClientUtils.h
@@ -132,6 +132,7 @@ public:
 	virtual unknown_ret DispatchClientSettingsChanged() = 0;
 	virtual unknown_ret DispatchClientPostMessage(const char*, const char*, const char*) = 0;
 	virtual unknown_ret IsSteamChinaLauncher() = 0;
+	virtual unknown_ret NeedsSteamChinaWorkshop() = 0;
 	virtual unknown_ret InitFilterText(uint32) = 0;
 	virtual unknown_ret FilterText(uint32, char*, uint32, const char*, bool) = 0;
 	virtual unknown_ret GetIPv6ConnectivityState( ESteamIPv6ConnectivityProtocol ) = 0;
